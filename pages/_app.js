59836e5ae02db3b306d5dae49bc7dc0735d71f4f
Gabriel Damian <gabrieldamian@Gabriels-MacBook-Air.local>
2025-02-23 12:28:08 +0200

    X

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
+      <Head>
         <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       </Head>
       <AlertProvider>
